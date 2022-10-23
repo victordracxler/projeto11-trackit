86768cf0f5475be503dfa5af98e4fe560adca1d7
Victor Dracxler <victordracxler@poli.ufrj.br>
2022-10-23 20:12:51 -0300

    style: login and signup pages bgcolor

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -86,6 +86,8 @@ const LoginWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  height: 100vh;
+  background-color: #FFFFFF;
 `;
 
 const LoginForm = styled.form`
